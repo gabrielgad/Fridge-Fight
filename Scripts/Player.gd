e99c3b1506b762af114a631ccb489d3284e01626
Gabe Home <gabrielgad911@yahoo.com>
2023-10-21 23:29:35 -0400

    completing new camera on springarm

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,31 +1,62 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var spring_arm_pivot = $SpringArmPivot
+@onready var spring_arm_3d = $SpringArmPivot/SpringArm3D
+@onready var rogue_hooded = $Rogue_Hooded
 
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+@export var MAX_SPEED = 20
+@export var ACCELERATION = 100
+@export var FRICTION = 80
+@onready var axis = Vector3.ZERO
 
-	# Handle Jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+const LERP_VAL = .05
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("Left", "Right", "Forward", "Back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("Quit"):
+		get_tree().quit()
+
+	if event is InputEventMouseMotion:
+		spring_arm_pivot.rotate_y(-event.relative.x * .005)
+		spring_arm_3d.rotate_x(-event.relative.y * .005)
+		spring_arm_3d.rotation.x = clamp(spring_arm_3d.rotation.x, -PI/4, PI/4)
+
+func _process(delta):
+	move(delta)
+
+
+func get_input_axis():
+	axis.x = Input.get_axis("Left", "Right")
+	axis.z = Input.get_axis("Forward", "Back")
+	axis = axis.rotated(Vector3.UP, spring_arm_pivot.rotation.y)
+	
+	return axis.normalized()
+	
+
+func move(delta):
+	axis = get_input_axis()
+	
+	if axis == Vector3.ZERO:
+		apply_fricition(FRICTION * delta)
+		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		apply_movement(axis * ACCELERATION * delta)
 
 	move_and_slide()
+
+
+func apply_fricition(amount):
+	if velocity.length() > amount:
+		velocity -= velocity.normalized() * amount
+	
+	else:
+		velocity = Vector3.ZERO
+
+func apply_movement(accel):
+	velocity += accel
+	velocity = lerp(velocity, velocity.limit_length(MAX_SPEED), LERP_VAL)
+	rogue_hooded.rotation.y = lerp_angle(rogue_hooded.rotation.y, atan2(velocity.x, velocity.z), LERP_VAL)
